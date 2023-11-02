6abfc4faa70f59a77b677eddfab76f0ba76da7ec
Steviggio <steve.mothmora@gmail.com>
2023-11-02 18:07:06 +0100

    Basic API structure with Mongoose

---
+++ src/Users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { User } from "./interfaces/user.interface";
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel("User") private readonly userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new this.userModel(createUserDto);
+    return user.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+}
