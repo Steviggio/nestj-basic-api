05bbf44228a927d206bcd2cb8295ef974b842a1d
Steviggio <steve.mothmora@gmail.com>
2023-11-02 18:09:04 +0100

    Correcting the localhost IP

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,6 @@ import { UsersModule } from './Users/users.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Steviggio_db'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/Steviggio_db'), UsersModule],
 })
 export class AppModule { }
